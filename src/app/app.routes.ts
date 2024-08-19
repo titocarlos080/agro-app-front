2f316c1db0b458c21f49cb0a6381a3f38a1c9081
Tito Carlos <titocarlos080@gmail.com>
2024-08-19 01:18:03 -0400

    second commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    { path: '', redirectTo: '/home', pathMatch: 'full' }, // Redirección a la ruta 'home'
+    { path: 'products', component: ProductsComponent }, // Ruta para productos
+    { path: 'home', component: HomeComponent }, // Ruta para home
+
+];
+
